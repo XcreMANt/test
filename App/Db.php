7ce38c445464321d42ccf085c5856c95055e6aaf
XcreMANt <kkdd@tut.by>
2019-06-21 00:09:34 +0300

    Merge branch 'php1/lesson8' of /home/steklotar/projects/test with conflicts.

---
+++ App/Db.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App;
+
+class Db
+{
+
+    protected $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = new \PDO('mysql:host=mysql;dbname=test', 'root', 'root');
+    }
+
+    public function execute($sql, $data = [])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($data);
+        return $res;
+    }
+
+    public function query($sql, $class, $data = [])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($data);
+        if (false !== $res) {
+            return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+        }
+        return [];
+    }
+
+}
